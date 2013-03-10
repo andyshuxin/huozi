f0bd6a147907fa5336873250cd13d96aacd43396
Andy Shu Xin <andyshuxin@gmail.com>
2013-03-11 04:44:28 +0800

    initial commit

---
+++ aep.py
@@ -0,0 +1,263 @@
+#coding=utf-8
+
+#AEP, Automatic E-magazine Processor, a set of modules that simulate
+#human action in fetching webpages, extracting main text and removing
+#unnecessary characters.
+#Currenly, the modules are customized for production of 1510 Weekly.
+
+__VERSION__ = 'Build 0001'
+__AUTHOR__ = "Andy Shu Xin (andy@shux.in)"
+__COPYRIGHT__ = "(C) 2013 Shu Xin. GNU GPL 3."
+
+import sys
+import codecs
+import string
+import urllib2
+from ExtMainText import extMainText
+from ExtMainText import get_text
+from bs4 import BeautifulSoup
+
+#####  Constants  #####
+
+DEBUG = True
+
+MAXSUBLEN = 12                     #Any line longer is assumed not subheadline
+SUBTHREDSOLD = 0.3
+
+CLEANERBOOK = (
+        (u'\u3000', ' '),          #"Ideographic" spaces
+        (u'\u00A0', ' '),          #&nbsp
+        (u'　', ' '),              #Full-width spaces
+        ('\r\n', '\n'),
+        ('\n ', '\n'),
+        #('\r\n ', '\r\n'),
+        ('\n\n', '\n'),            #duplicate paragraph breaks
+        (u'\ue5f1', ''),           #Unknown weird character in CJK
+        ('......',  u'……'),
+        ('...',  u'……'),
+        (u'。。。。。。',  u'……'),
+        (u'。。。',  u'……'),
+        (u'--', u'——'),
+        (u'－－', u'——'),
+        (u'■', ''),
+        )
+
+CLEANER_EXCEPTION_BOOK = {}
+
+#####  Data strcture  #####
+
+class Article(object):
+
+    def __init__(self, title='', author='', text='',
+                 subheadLines=[], category = '', portraitPath=''):
+        self.title = title
+        self.author = author
+        self.text = text
+        self.subheadLines = subheadLines
+        self.portraitPath = portraitPath
+
+    def __str__(self):
+        return '====Article====\n' + \
+               'Title is '+self.title+'\n'+ \
+               'Author is '+self.author+'\n'+ \
+               '***Main text starts ***\n' + self.text + '***Main text ends ***\n' + \
+               'Subhead line numbers: ' + str(self.subheadLines)
+
+class Issue(object):
+
+    def __init__(self, issueNum='', grandTitle='', ediRemark=''):
+        self.issueNum = issueNum
+        self.grandTitle = grandTitle
+        self.ediRemark = ediRemark
+        self.articleList = []
+        self.categoryList = []
+
+    def addArticle(self, article):
+        self.articleList.append(article)
+
+    def deleteArticle(self, article):
+        self.articleList.remove(article)
+
+    def __str__(self):
+        res = 'Issue no.'+unicode(self.issueNum)+' '+self.grandTitle+'\n'+\
+               'total article number:'+unicode(len(self.articleList))+'\n'+\
+               'Editor says:'+self.ediRemark+'\n'
+        for article in self.articleList:
+            res += unicode(article) + '\n'
+        return res
+
+    #def addCategory(self, word, articlesContained):
+        #newCategory = Category(word, articlesContained)
+        #self.categoryList.append(newCategory)
+
+##########  Clearner module  ##########
+
+def _isChinese(char):
+    return 0x4e00 <= ord(char) < 0x9fa6
+
+def cleanText(input, patternBook=None):
+    if patternBook == None:
+        patternBook = CLEANERBOOK
+
+    text = input
+    for patternPair in patternBook:
+        key = patternPair[0]
+        while string.find(text, key) != -1:
+            text = string.replace(text, key, patternPair[1])
+
+    ## Kill spaces and blank lines at BOF
+    while text[0] == ' ' or text[0] == '\n':
+        text = text[1:]
+    while text[0:2] == '\r\n':
+        text = text[2:]
+
+    ## Dealing with spaces
+    i = 1
+    while i < len(text) - 1:
+        if text[i] == ' ':
+
+            # Delete spaces around Chinese characters
+            if _isChinese(text[i-1]) or _isChinese(text[i+1]):
+                text = text[:i] + text[i+1:]
+
+            # Combine multiple consecutive spaces
+            elif (text[i-1] == ' ') or (text[i+1] == ' '):
+                text = text[:i] + text[i+1:]
+
+        i += 1
+
+    return text
+
+#####  Parser Module  #####
+
+def _guessTitle(htmlText):
+    titleStart = string.find(htmlText, '<title')
+    titleEnd = string.find(htmlText, '</title>')
+    leftTagEnd = string.find(htmlText, '>', titleStart)
+    if leftTagEnd+1 <= titleEnd:
+        title = htmlText[leftTagEnd+1:titleEnd]
+    else:
+        title = '*****'
+    return title
+
+def _guessAuthor(htmlText):
+    #TODO
+    return ''
+
+def _guessSubhead(htmlText, mainText):
+    #TODO: (a) use <h3> stuff (b) use length
+    subs = []
+    lines = cleanText(mainText).split('\n')
+
+    for lineNo in range(1, len(lines) - 1):
+        L = len(lines[lineNo])
+        Lup = len(lines[lineNo-1])
+        Ldown = len(lines[lineNo+1])
+
+        if (L < SUBTHREDSOLD * (Lup + Ldown) / 2) and (L < MAXSUBLEN):
+            subs.append(lineNo+1)
+    return subs
+
+def parseHtml(htmlText):
+
+    """
+    Input: htmlText, a string, which is a html file
+    Output: a tuple, first element being the content part of the html,
+            and the second element being a dictionary mapping meta labels
+            with their values
+    """
+
+    mainText = extMainText(htmlText)
+    ratio = 0.5 - 0.1
+    while len(get_text(mainText)) < 10:
+        mainText = extMainText(htmlText, ratio, True)
+        ratio -= 0.1
+    mainText = get_text(mainText)
+
+    meta = {
+            'title': _guessTitle(htmlText),
+            'author': _guessAuthor(htmlText),
+            'sub': _guessSubhead(htmlText, mainText),
+            }
+
+    return (mainText, meta)
+
+#####  Grabber module  #####
+def grab(url):
+
+    def _isLegit(char):
+
+        """
+        Return true if char can be part of the name of a charset
+        """
+
+        if char in string.ascii_letters or \
+           char in string.digits or \
+           char == '-':
+           return True
+        else:
+           return False
+
+    def _urlClean(url):
+        #TODO: transform renren.com -> www.renren.com
+        res = url
+        if url[:4] != 'http':
+            res = 'http://www.' + url
+        return res
+
+    #End internal func
+
+    url = _urlClean(url)
+
+    hdr = {'User-Agent': 'Mozilla/5.0'}
+    req = urllib2.Request(url, headers=hdr)
+    try:
+        html = urllib2.urlopen(req).read()
+    except:
+        pass #TODO
+
+    #Handle encoding
+    csPos = html.index('charset=') + len('charset=')
+    while not _isLegit(html[csPos]):
+        csPos += 1
+    L = 1
+    while _isLegit(html[csPos + L]):
+        L += 1
+    charset = html[csPos:csPos+L]
+    if charset == 'gb2312':       #just to be safe
+        charset = 'gbk'
+    try:
+        html = html.decode(charset, 'ignore')
+        return html
+    except UnicodeDecodeError, err:
+        #TODO
+        print err
+        return html
+
+#####  File operation module  ####
+def readfile(filename):
+    f = codecs.open(filename, 'r', 'utf-8')
+    text = f.read()
+    f.close()
+    return text
+
+def writefile(filename, content):
+    f = codecs.open(filename, 'w', 'utf-8')
+    f.write(content)
+    f.close()
+
+def readArgv(n):
+    '''
+    n: number arguments expected to be returned
+    Return: a tuple of n length consisting of arguments,
+    if argument is inadequate, use None to fill in.
+    '''
+    res = []
+    for i in range(1, len(sys.argv)):
+        res.append(sys.argv[i])
+    if len(res) > n:
+        return res[:n]
+    elif len(res) == n:
+        return tuple(res)
+    else:
+        return tuple(res) + (None,) * (n - len(res))
---
+++ doc.py
@@ -0,0 +1,147 @@
+#!python
+#coding=utf-8
+
+from urllib import urlopen
+from aep import Article, Issue
+from aep import grab, parseHtml, cleanText
+import codecs
+
+try:
+    import win32com.client as win32
+except:
+    pass
+
+## Constants
+FONT = u'宋体'
+
+
+def createDoc(issue):
+    word = win32.gencache.EnsureDispatch('Word.Application')
+    doc = word.Documents.Add()
+    #doc = word.Documents.Open('C:\\Dropbox\\aep\\template.doc')
+    word.Visible = True
+
+    ## Set up styles
+
+    # Normal Text 
+    font = doc.Styles(win32.constants.wdStyleNormal).Font
+    font.Size = 11
+    font.Name = FONT
+    paraF = doc.Styles(win32.constants.wdStyleNormal).ParagraphFormat
+    paraF.LineSpacing = 1.5 * font.Size
+    paraF.CharacterUnitFirstLineIndent = 2   # Heading 1
+    font = doc.Styles(win32.constants.wdStyleHeading1).Font
+    font.Bold = True
+    font.Size = 24
+    font.Name = FONT
+    paraF = doc.Styles(win32.constants.wdStyleHeading1).ParagraphFormat
+    paraF.Alignment = win32.constants.wdAlignParagraphLeft
+    paraF.CharacterUnitFirstLineIndent = 0
+
+    # Heading 2
+    font = doc.Styles(win32.constants.wdStyleHeading2).Font
+    font.Bold = True
+    font.Size = 18
+    font.Name = FONT
+    paraF = doc.Styles(win32.constants.wdStyleHeading2).ParagraphFormat
+    paraF.Alignment = win32.constants.wdAlignParagraphCenter
+    paraF.CharacterUnitFirstLineIndent = 0
+
+    # Subheads
+    font = doc.Styles(win32.constants.wdStyleHeading3).Font
+    font.Bold = True
+    font.Italic = False
+    font.Size = 11
+    font.Name = FONT
+    paraF = doc.Styles(win32.constants.wdStyleHeading3).ParagraphFormat
+    paraF.Alignment = win32.constants.wdAlignParagraphCenter
+    paraF.LineSpacing = 2 * font.Size
+    paraF.CharacterUnitFirstLineIndent = 0
+
+    # Foot notes
+    font = doc.Styles(win32.constants.wdStyleQuote).Font
+    font.Bold = True
+    font.Size = 10
+    font.Name = FONT
+
+    ## Add Header
+    C = win32.constants.wdHeaderFooterPrimary
+    hdr = u'一五一十周刊第' + issue.issueNum + u'期' + \
+          u'： ' + issue.grandTitle
+    word.ActiveDocument.Sections(1).Headers(C).Range.Text = hdr
+
+    rng = doc.Range(0, 0)
+
+    ## Add Cover page
+    rng.InsertAfter(u'【封面圖片】\r\n')
+    rng.Collapse( win32.constants.wdCollapseEnd )
+    rng.InsertBreak( win32.constants.wdPageBreak )
+
+    ## Add Editor's Remark
+    rng.InsertAfter(u'【编者的话】\r\n')
+    rng.InsertAfter(issue.ediRemark)
+    rng.Collapse( win32.constants.wdCollapseEnd )
+    rng.InsertBreak( win32.constants.wdPageBreak )
+
+    ## Add Contents Page
+    rng.InsertAfter(u'目录\r\n')
+    rng.Collapse( win32.constants.wdCollapseEnd )
+    tocPos = rng.End
+    rng.InsertBreak( win32.constants.wdPageBreak )
+
+    #rng.Collapse( win32.constants.wdCollapseEnd )
+    #articleStart = rng.End
+
+    ## Add articles
+    for article in issue.articleList:
+        # Title
+        rng.InsertAfter(article.author + u'：' + article.title + '\r\n')
+        rng.Style = win32.constants.wdStyleHeading2
+        rng.Collapse( win32.constants.wdCollapseEnd )
+
+        # Main text
+        lineNo = 0
+        for line in article.text.split('\n'):
+            rng.Collapse( win32.constants.wdCollapseEnd )
+            rng.InsertAfter(line+'\r\n')
+            if lineNo + 1 in article.subheadLines:
+                rng.Style = win32.constants.wdStyleHeading3
+            lineNo += 1
+        rng.Collapse( win32.constants.wdCollapseEnd )
+        rng.InsertBreak( win32.constants.wdPageBreak )
+
+    ## About infomation
+    ## TODO
+
+    #Update TOC
+    doc.TablesOfContents.Add(doc.Range(tocPos, tocPos), True, 1, 2)
+    #doc.TablesOfContents(1).Update()
+
+    #Unify font, just in case
+    rng = doc.Range()
+    rng.Font.Name = FONT
+
+    #doc.Close(False)
+    #word.Application.Quit()
+
+def test():
+    issue = Issue()
+    issue.issueNum = '666'
+    issue.grandTitle = u'测试刊'
+
+    article1 = Article()
+    url = 'http://my1510.cn/article.php?id=94381'
+    html = grab(url)
+    parsed = parseHtml(html)
+    article1.text = cleanText(parsed[0])
+    article1.title = parsed[1]['title']
+    article1.author = parsed[1]['author']
+    article1.subheadLines = parsed[1]['sub']
+
+    #print article1.subheadLines
+
+    issue.addArticle(article1)
+    createDoc(issue)
+
+if __name__ == '__main__':
+    test()
