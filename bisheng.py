189a0d7299766205c32c476dc8524b74c42f5c98
Andy Shu Xin <andyshuxin@gmail.com>
2013-03-11 17:23:33 +0800

    improve docstrings, comments, and styles

--- bisheng.py
+++ bisheng.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 #coding=utf-8
 
-# Huozi, GUI for AEP module; currently customized for production
+# Bisheng, GUI for AEP module; currently customized for production
 # of 1510 Weekly.
 __VERSION__ = 'Build 0001'
 from aep import __VERSION__ as __AEPVERSION__
@@ -37,8 +37,9 @@ txt = {
        'btnDn':         "&Down",
        'btnMdf':        "&Modify",
        'btnDel':        "D&elete",
-       'graberror':     "Something is wrong grabbing:",
+       'graberror':     "Something is wrong grabbing: ",
        'graberrorCap':  "Grabber Error",
+       'tglSubhead':    "Toggle sub&headline",
        }
 
 #####  UI  #####
@@ -56,11 +57,10 @@ class HuoziMainFrame(wx.Frame):
 
     def InitUI(self):
 
-
-        #Panel, sole and only
+        # Panel, sole and only
         self.panel = wx.Panel(self, wx.ID_ANY)
 
-        #Box sizers
+        # Box sizers
         hBox = wx.BoxSizer(wx.HORIZONTAL)
         vBoxLeft = wx.BoxSizer(wx.VERTICAL)
         vBoxRight = wx.BoxSizer(wx.VERTICAL)
@@ -70,8 +70,25 @@ class HuoziMainFrame(wx.Frame):
         vBoxLeft.Add(gridBox, proportion=0, flag=wx.EXPAND)
         self.panel.SetSizerAndFit(hBox)
 
-        #Information bars
-        ##TODO: modulize
+        # Toolbar
+        self.toolbar = self.CreateToolBar()
+        configIssueTool = self.toolbar.AddLabelTool(wx.ID_SETUP, 'ConfigureIssue', wx.Bitmap('img/configissue.png'))
+        addArticleTool = self.toolbar.AddLabelTool(wx.ID_ADD, 'AddArticle', wx.Bitmap('img/addarticle.png'))
+        getDocTool = self.toolbar.AddLabelTool(wx.ID_ANY, 'Generate MS Word', wx.Bitmap('img/spare.png'))
+        quitTool = self.toolbar.AddLabelTool(wx.ID_EXIT, 'Quit', wx.Bitmap('img/exit.png'))
+
+        self.Bind(wx.EVT_TOOL, self.OnConfigIssue, configIssueTool)
+        self.Bind(wx.EVT_TOOL, self.OnAddArticles, addArticleTool)
+        self.Bind(wx.EVT_TOOL, self.OnCreateDoc, getDocTool)
+        self.Bind(wx.EVT_TOOL, self.OnQuit, quitTool)
+        if not DEBUG:
+            self.toolbar.EnableTool(wx.ID_ADD, False)        #Enable after issue configured
+        if os.name != 'nt':  #Is not Windows
+            self.toolbar.EnableTool(getDocTool.Id, False)
+        self.toolbar.Realize()
+
+        # Information bars
+        # TODO: better formatting and add direct editability
         self.infoBar1 = wx.StaticText(self.panel, wx.ID_ANY, '',
                                 wx.DefaultPosition, wx.DefaultSize,
                                 style=wx.ALIGN_LEFT|wx.ALIGN_TOP)
@@ -85,7 +102,7 @@ class HuoziMainFrame(wx.Frame):
         vBoxRight.Add(self.infoBar2, 0, flag=wx.TOP|wx.EXPAND)
         vBoxRight.Add(self.infoBar3, 0, flag=wx.TOP|wx.EXPAND)
 
-        #Article List
+        # Article List
         self.articleList = wx.ListBox(self.panel, wx.ID_ANY, wx.DefaultPosition,
                                       wx.DefaultSize, self.issue.articleList)
         vBoxLeft.Add(self.articleList, proportion=1, flag=wx.EXPAND)
@@ -94,7 +111,7 @@ class HuoziMainFrame(wx.Frame):
         self.Bind(wx.EVT_LISTBOX_DCLICK, self.OnArticleListDclick,
                   self.articleList)
 
-        #Buttons
+        # Buttons to manipulate articles
         self.btnUp = wx.Button(self.panel, wx.ID_UP, txt['btnUp'])
         self.btnDn = wx.Button(self.panel, wx.ID_DOWN, txt['btnDn'])
         self.btnMdf = wx.Button(self.panel, wx.ID_ANY, txt['btnMdf'])
@@ -107,16 +124,16 @@ class HuoziMainFrame(wx.Frame):
         self.Bind(wx.EVT_BUTTON, self.OnModify, self.btnMdf)
         self.Bind(wx.EVT_BUTTON, self.OnDelete, self.btnDel)
 
-        #Maintext Window
+        # Maintext display and editing 
         self.textBox = wx.TextCtrl(self.panel,
-                                   value='Select articles to view content',
+                                   value='',
                                    style=wx.TE_MULTILINE|wx.TE_RICH2)
         vBoxRight.Add(self.textBox, 1, flag=wx.EXPAND|wx.BOTTOM|wx.TE_BESTWRAP)
         self.Layout()
         self.Bind(wx.EVT_TEXT, self.OnTextBoxChanged, self.textBox)
 
-        #Maintext Buttons 
-        btnSubhead = wx.Button(self.panel, -1, 'Toggle sub&headline')
+        # Buttons to manipulate the text
+        btnSubhead = wx.Button(self.panel, -1, txt['tglSubhead'])
         hBoxBottom = wx.BoxSizer(wx.HORIZONTAL)
         hBoxBottom.Add(btnSubhead, 0)
         vBoxRight.Add(hBoxBottom, 0)
@@ -126,25 +143,6 @@ class HuoziMainFrame(wx.Frame):
             btnDev = wx.Button(self.panel, -1, '[DEV]Print issue to CLI')
             hBoxBottom.Add(btnDev, 0)
             self.Bind(wx.EVT_BUTTON, self.printIssue, btnDev)
-
-        #Toolbar
-        self.toolbar = self.CreateToolBar()
-        configIssueTool = self.toolbar.AddLabelTool(wx.ID_SETUP, 'ConfigureIssue', wx.Bitmap('img/configissue.png'))
-        addArticleTool = self.toolbar.AddLabelTool(wx.ID_ADD, 'AddArticle', wx.Bitmap('img/addarticle.png'))
-        getDocTool = self.toolbar.AddLabelTool(wx.ID_ANY, 'Generate MS Word', wx.Bitmap('img/spare.png'))
-        quitTool = self.toolbar.AddLabelTool(wx.ID_EXIT, 'Quit', wx.Bitmap('img/exit.png'))
-
-        self.Bind(wx.EVT_TOOL, self.OnConfigIssue, configIssueTool)
-        self.Bind(wx.EVT_TOOL, self.OnAddArticles, addArticleTool)
-        self.Bind(wx.EVT_TOOL, self.OnGetDoc, getDocTool)
-        self.Bind(wx.EVT_TOOL, self.OnQuit, quitTool)
-        if not DEBUG:
-            self.toolbar.EnableTool(wx.ID_ADD, False)        #Enable after issue configured
-        if os.name != 'nt':  #Is not Windows
-            self.toolbar.EnableTool(getDocTool.Id, False)
-
-        self.toolbar.Realize()
-
 
         #Main window
         self.SetSize((800, 600))
@@ -157,6 +155,10 @@ class HuoziMainFrame(wx.Frame):
         self.firstConfig = True
 
     def OnConfigIssue(self, e):
+
+        """
+        Set the basic information of the current issue.
+        """
 
         issueNum = self.askInfo(txt['issueNumQ'],
                                 txt['issueNumT'],
@@ -190,6 +192,11 @@ class HuoziMainFrame(wx.Frame):
                       self.issue.grandTitle)
 
     def OnAddArticles(self, e):
+
+        """
+        Ask for a list of URLs and retrieve and process them, and update the article list.
+        """
+
         urlText = self.askInfo(txt['AddArticleQ'],
                                txt['AddArticleT'],
                                multiline=True)
@@ -223,7 +230,7 @@ class HuoziMainFrame(wx.Frame):
             self.articleList.Insert(article.title, pos)
             pos += 1
 
-    def OnGetDoc(self, e):
+    def OnCreateDoc(self, e):
         createDoc(self.issue)
 
     def OnQuit(self, e):
@@ -332,19 +339,17 @@ class HuoziMainFrame(wx.Frame):
     def OnToggleSubhead(self, e):
 
         """
-        If ToggleHighlight is clicked:
-            1. if the current line is not highlighted, highlight it,
-               and add info to Article instance, or
-            2. if the current line is already highlighted, do the opposite.
+        Toggles highlight of the current line and
+        updates subheadLines information of the selected article.
         """
 
         #XXX For some weird reason, textBox.GetInsertionPoint is never -1.
-        #    When nothing is selected, it's 0. Obviously This is a compromise,
-        #    because when the user points the cursor to the begginning of
-        #    the file, togglesubhead is still disabled.
+        #    When the focus is off, it's 0. 
+
         if self.textBox.GetInsertionPoint() == 0:
             return
 
+        #Get selected article
         lineNo = len(self.textBox.GetRange(0, self.textBox.GetInsertionPoint()).split("\n"))
         if self.articleList.GetSelection() != -1:   #TODO: modulize
             selectedTitle = self.articleList.GetStringSelection()
@@ -354,7 +359,7 @@ class HuoziMainFrame(wx.Frame):
                     break
 
         if lineNo in article.subheadLines:
-            #UI action dehighlight the line
+            #UI action: dehighlight the line
             pos = self.textBox.GetInsertionPoint()
             fulltext = self.textBox.GetRange(0, self.textBox.GetLastPosition())
             try:
@@ -387,12 +392,14 @@ class HuoziMainFrame(wx.Frame):
             article.subheadLines.append(lineNo)
 
     def updateInfoBar(self, index):
+
         if self.issue.articleList == []:
             return
 
-        display1 = 'Issue no.'+str(self.issue.issueNum)+' '+self.issue.grandTitle+\
+        issueInfo = 'Issue no.'+str(self.issue.issueNum)+' '+self.issue.grandTitle+\
                   ' Total number of articles:'+str(len(self.issue.articleList))
-        self.infoBar1.SetLabel(display1)
+        self.infoBar1.SetLabel(issueInfo)
+
         if index == -1:
             return
         selectedTitle = self.articleList.GetString(index)
@@ -402,12 +409,14 @@ class HuoziMainFrame(wx.Frame):
                 break
         title = chosenArticle.title
         author = chosenArticle.author
-        self.infoBar2.SetLabel('Article title: '+title)
-        self.infoBar3.SetLabel('Author: '+author)
+        self.infoBar2.SetLabel('Article title: ' + title)
+        self.infoBar3.SetLabel('Author: ' + author)
 
     def updateTextBox(self, index):
+
         if self.articleList.GetSelection() == -1:
             return
+
         title = self.articleList.GetStringSelection()
         for article in self.issue.articleList:
             if article.title == title:
@@ -437,6 +446,10 @@ class HuoziMainFrame(wx.Frame):
 
 
     def askInfo(self, prompt, dialogTitle, defaultVal='', multiline=False, noCancel=False):
+
+        """
+        Open a dialog and ask the user for information.
+        """
 
         style = wx.OK
         if multiline:
